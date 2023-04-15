946f1bd5bee672b953bdb6f6d35ee6b1c33a1d91
Damian Stasik <dstasik@student.agh.edu.pl>
2023-04-15 15:31:16 +0200

    Added passwords hashing while creating new user

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -4,6 +4,7 @@ import com.company.enroller.model.Participant;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -12,6 +13,9 @@ import java.util.Collection;
 public class ParticipantService {
 
 	DatabaseConnector connector;
+
+	@Autowired //automatyczna inicjalizacja instancji obiektu poprzez Bean
+	PasswordEncoder passwordEncoder;
 
 	public ParticipantService() {
 		connector = DatabaseConnector.getInstance();
@@ -42,6 +46,9 @@ public class ParticipantService {
 	}
 
 	public Participant add(Participant participant) {
+		String hashedPassword = passwordEncoder.encode(participant.getPassword());
+		participant.setPassword(hashedPassword);
+
 		Transaction transaction = connector.getSession().beginTransaction();
 		connector.getSession().save(participant);
 		transaction.commit();
