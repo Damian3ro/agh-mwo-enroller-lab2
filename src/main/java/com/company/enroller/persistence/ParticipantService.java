f32ae0bebbd2b1e50c66c618a1f49fb16da98b2f
Damian Stasik <dstasik@student.agh.edu.pl>
2023-04-15 13:51:34 +0200

    Sorting and key added

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Participant;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +18,23 @@ public class ParticipantService {
 	}
 
 	public Collection<Participant> getAll() {
-		return connector.getSession().createCriteria(Participant.class).list();
+		String hql = "FROM Participant";
+		return connector.getSession().createQuery(hql).list();
+	}
+
+	public Collection<Participant> getAll(String sortBy, String sortOrder, String key) {
+		String hql = "FROM Participant";
+		if (!key.equals("")) {
+			hql += " WHERE login LIKE '%" + key + "%'";
+		}
+		if (sortBy.equals("login")) {
+			hql += " ORDER BY login";
+			if (sortOrder.equals("ASC") || sortOrder.equals("DESC")) {
+				hql += " " +  sortOrder;
+			}
+		}
+		Query query = connector.getSession().createQuery(hql);
+		return query.list();
 	}
 
 	public Participant findByLogin(String login) {
